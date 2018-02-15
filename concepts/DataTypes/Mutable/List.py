e2cb38abfbba74627a2508f29c5d654c260dc1ca
Muhammad Faisal Haider <faisal.haider@royalcyber.net>
2018-02-15 16:36:23 +0500

    List in Python

--- concepts/DataTypes/Mutable/List.py
+++ concepts/DataTypes/Mutable/List.py
@@ -25,3 +25,23 @@ print(egList[3:-6])
 
 # Repetition
 print(egList * 2)
+
+# Type Specific functions
+
+# append(value) [It takes only one argument i.e. value itself and adds it at the last]
+list = [1, 2, 'many']
+list.append('one to one')
+print(list)
+
+# extend(list) [It also takes only one argument either element or list]
+list.extend('join')  # If extended value is in string and more than one characters then it breaks it down as list
+list.extend(['many', 2, 1.0])
+print(list)
+
+# insert() [It takes exact two arguments, first index, and second value]
+list.insert(4, 'few')  # It doesn't replace previous value instead just add new value at specified index and updates it
+print(list)
+
+# pop() [Removes last element from the list]
+list.pop()
+print(list)
