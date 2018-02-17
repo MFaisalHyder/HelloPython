4bda916933d75348a96b6a24cbf43bc754451acb
MFaisalHyder <faisi94@gmail.com>
2018-02-17 22:52:32 +0500

    Learning Python - DataType {Dictionary}

--- concepts/DataTypes/Mutable/Dictionary.py
+++ concepts/DataTypes/Mutable/Dictionary.py
@@ -8,5 +8,19 @@ egDictionary = {1: 'Programming Languages', 'first': 'Java', 'second': 'Python'}
 print(egDictionary)
 
 # In case of duplicates, latest value wins
-egDictionary = {1: 'Programming Languages', 'first' : 'Java', 'second' : 'Python', 'second' : 'C#'}
+egDictionary = {1: 'Programming Languages', 'first': 'Java', 'second': 'Python', 'second': 'C#'}
 print(egDictionary)
+
+# Dictionary Methods
+# index [KeyError if given wrong index]
+# print(egDictionary[5])
+print(egDictionary['second'])
+
+# len()
+print(len(egDictionary))
+
+# key()
+print(egDictionary.keys())
+
+# values()
+print(egDictionary.values())
