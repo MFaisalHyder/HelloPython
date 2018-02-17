31f94e1dc9f64dd1b45c34c0345a7192622f5f5c
MFaisalHyder <faisi94@gmail.com>
2018-02-17 22:23:45 +0500

    Learning Python - DataType {Dictionary}

--- concepts/DataTypes/Mutable/Dictionary.py
+++ concepts/DataTypes/Mutable/Dictionary.py
@@ -1,4 +1,12 @@
 """
 It is most flexible data type in PYTHON
 Stores value in Key Value pair.
+egDictionary = {1: 'Programming Languages', 'first' : 'Java', 'second' : 'Python'}
 """
+
+egDictionary = {1: 'Programming Languages', 'first': 'Java', 'second': 'Python'}
+print(egDictionary)
+
+# In case of duplicates, latest value wins
+egDictionary = {1: 'Programming Languages', 'first' : 'Java', 'second' : 'Python', 'second' : 'C#'}
+print(egDictionary)
