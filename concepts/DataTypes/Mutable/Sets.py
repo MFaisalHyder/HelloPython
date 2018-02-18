2d393540fce61bb04a7ccd4008069fc2ebc4b4ca
MFaisalHyder <faisi94@gmail.com>
2018-02-18 21:13:31 +0500

    Learning Python - DataType {Sets}

--- concepts/DataTypes/Mutable/Sets.py
+++ concepts/DataTypes/Mutable/Sets.py
@@ -5,3 +5,16 @@ egSet = {1,2,'Faisal'}
 
 egSet = {10, 5, 2, 'Hyder', 3, 2, 'Zinc', 'Faisal'}
 print(egSet)
+
+set1 = {1, 3, 5, 9, 0, 6}
+set2 = {2, 3, 7, 8, 6}
+
+# Operations
+# Union [|, common to arithmetic's union operation]
+print(set1 | set2)
+
+# Intersection [&, common to arithmetic's union operation]
+print(set1 & set2)
+
+# Difference [-, common to arithmetic's union operation]
+print(set1 - set2)
