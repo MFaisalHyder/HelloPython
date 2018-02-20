6ccc00c50b2fbb791de83224c4d42f35ba5e6e49
MFaisalHyder <faisi94@gmail.com>
2018-02-20 12:20:54 +0500

    Learning Python - Flow Controlling in Program

--- concepts/FlowControl/Conditions.py
+++ concepts/FlowControl/Conditions.py
@@ -1,3 +1,16 @@
 """
 If, else, elif
 """
+
+num1 = 5
+num2 = 10
+
+# If elif else
+if num1 == num2:
+    print('It is equal')
+
+elif num2 % num1 == 0:
+    print('Completely divisible')
+
+else:
+    print('Not equal')
