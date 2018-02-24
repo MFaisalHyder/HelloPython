37c5ec69e66bffeaf645b40cf068deed90f4f78e
MFaisalHyder <faisi94@gmail.com>
2018-02-24 23:48:53 +0500

    OOP Concepts - Variables

--- Concepts/OOP/OOP Concepts.py
+++ Concepts/OOP/OOP Concepts.py
@@ -1,5 +1,7 @@
 # [class]
 class Employee:
+    # class variable, instances can also access it, if the same is not defined for their own scope
+    raise_amount = 1.05
 
     # [function] This is predefined method which is equivalent to Constructor in Java
     def __init__(self, first_name, middle_name, last_name, employee_number, employee_designation, salary):
@@ -14,9 +16,22 @@ class Employee:
     def employee_info(self):
         return '{} {} working as {}'.format(self.firstName, self.lastName, self.designation)
 
+    def increment(self):
+        self.salary = int(Employee.raise_amount * self.salary)
+
 
 employeeFaisal = Employee('Muhammad', 'Faisal', 'Hyder', 'RC_10000386', 'Software Engineer', 99999999)
+employeeShane = Employee('Shane', 'Mathew', 'Corey', 'AV_10000886', 'Software Engineer', 85000)
 
 print(employeeFaisal.employee_info())
 # methods are transformed into class_name.method_name(instance)
 # i.e. Employee.employee_info(employeeFaisal)
+
+employeeShane.raise_amount = 1.04
+print("Faisal's salary", employeeFaisal.salary)
+print("Increment for Faisal : ", employeeFaisal.raise_amount, employeeFaisal.increment())
+print("Faisal's salary after increment", employeeFaisal.salary)
+
+print("Shane's salary", employeeShane.salary)
+print("Increment for Shane : ", employeeShane.raise_amount, employeeShane.increment())
+print("Shane's salary after increment", employeeShane.salary)
