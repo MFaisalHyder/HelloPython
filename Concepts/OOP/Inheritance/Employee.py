c45106d98a5ad9052b2ee908e940237ad1e655d2
MFaisalHyder <faisi94@gmail.com>
2018-03-05 17:03:40 +0500

    OOP Concepts [Inheritance]

--- Concepts/OOP/Inheritance/Employee.py
+++ Concepts/OOP/Inheritance/Employee.py
@@ -17,9 +17,38 @@ class Employee:
 class Developer(Employee):
     raise_amount = 1.15
 
+    # Constructor of Developer class
+    def __init__(self, first_name, middle_name, last_name, employee_number, employee_designation, salary, programming_lang):
+        pass
+        # Now there are two ways to let Parent's constructor handle initialization of common field, this way we achieve DRY principle
+        # 1st
+        super().__init__(first_name, middle_name, last_name, employee_number, employee_designation, salary)
+        # 2nd
+        # Employee.__init__(self, first_name, middle_name, last_name, employee_number, employee_designation, salary)
+        self.programming_lang = programming_lang
 
-faisalDev = Employee('Muhammad', 'Faisal', 'Hyder', 'RC_10000386', 'Software Engineer', 81000)
-# faisalDev = Developer('Muhammad', 'Faisal', 'Hyder', 'RC_10000386', 'Software Engineer', 81000)
+
+class Manager(Employee):
+    def __init__(self, first_name, middle_name, last_name, employee_number, employee_designation, salary, employees_list=None):
+        # it is better to set mutable data types with None in parameters list
+        super().__init__(first_name, middle_name, last_name, employee_number, employee_designation, salary)
+        if employees_list is None:
+            self.employees_list = []
+        else:
+            self.employees_list = employees_list
+
+    def hire_employee(self, employee):
+        if employee not in self.employees_list:
+            self.employees_list.append(employee)
+
+    def terminate_employee(self, employee):
+        if employee in self.employees_list:
+            self.employees_list.remove(employee)
+
+
+faisalDev = Employee('Muhammad', 'Faisal', 'Hyder', 'RC_10000386', 'Software Engineer', 165000)
+faisalPythonDev = Developer('Muhammad', 'Faisal', 'Hyder', 'PyLab_11', 'Software Engineer', 165000, 'Python')
+faisalJavaDev = Developer('Muhammad', 'Faisal', 'Hyder', 'Av_11', 'Software Engineer', 185000, 'Java')
 
 # This functions helps to visualize easily the hierarchy of inherited class and Method resolution order
 # print(help(Developer))
