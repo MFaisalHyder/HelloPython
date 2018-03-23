ce74bd30e00c29b3ff0e1cb9ab1c1b8366eb75a0
MFaisalHyder <faisi94@gmail.com>
2018-03-23 23:43:50 +0500

    OOP [Property Decorators]

---
+++ Concepts/OOP/PropertyDecorators/Decorators.py
@@ -0,0 +1,5 @@
+class Employee:
+    def __init__(self, first_name, last_name, designation):
+        self.firstName = first_name
+        self.lastName = last_name
+        self.designation = designation
