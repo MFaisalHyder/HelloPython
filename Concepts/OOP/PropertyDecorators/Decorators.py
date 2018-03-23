a4a9ef69008a40923261e8db154a9fb44799e610
MFaisalHyder <faisi94@gmail.com>
2018-03-24 00:01:56 +0500

    OOP [Property Decorators]

--- Concepts/OOP/PropertyDecorators/Decorators.py
+++ Concepts/OOP/PropertyDecorators/Decorators.py
@@ -8,6 +8,21 @@ class Employee:
     def email(self):
         return '{}.{}@email.com'.format(self.firstName, self.lastName)
 
+    @property
+    def full_name(self):
+        return '{} {}'.format(self.firstName, self.lastName)
+
+    @full_name.setter
+    def full_name(self, full_name):
+        first_name, last_name = full_name.split(' ')
+        self.firstName = first_name
+        self.lastName = last_name
+
+    @full_name.deleter
+    def full_name(self):
+        self.firstName = None
+        self.lastName = None
+
 
 emp1 = Employee('Faisal', 'Hyder', 'Java Dev')
 print(emp1.email)
